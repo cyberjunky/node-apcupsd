509993e7a35cc0770c1b8550f55f4687edfc21c9
Ron Klinkien <ron@cyberjunky.nl>
2014-05-17 14:55:27 +0200

    Added and changed License. Added gitignore file.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /*
 
-   Script to poll an APC UPS (via apcupsd) and publish interesting values to MQTT.
-   Published under GPL3 by Ron Klinkien <support@domotiga.nl>
-   Copyright (C) 2014-05-16 The Netherlands
+   App to poll an APC UPS (via apcupsd) and publish interesting values to MQTT.
+   Published under MIT license by Ron Klinkien <ron@cyberjunky.nl>
+   Copyright (C) 2014 The Netherlands
 
 */
 
@@ -16,66 +16,66 @@ var curvalues = {}; // holds current values
 
 function executeCmd(cmd, callback) {
 
-  exec(cmd, function (err, stdout, stderror) {
-    // console.log('stdout: %s', output);
-    if (err) {
-      callback(err);
-    } 
-    else if (stderror) {
-      callback(stderror);
-    }
-    else {
-      if (stdout) {
-        callback(null,stdout);
+    exec(cmd, function (err, stdout, stderror) {
+      // console.log('stdout: %s', output);
+      if (err) {
+        callback(err);
       } 
-      else {
-        callback(null, null);
+      else if (stderror) {
+        callback(stderror);
       }
-    }
-  });
+      else {
+        if (stdout) {
+          callback(null,stdout);
+        } 
+        else {
+          callback(null, null);
+        }
+      }
+    });
 }
 
 function poll() {
 
-  var wanted = ['upsname', 'serialno', 'status', 'linev', 'linefreq', 'loadpct', 'battv', 'bcharge'];
+    var wanted = ['upsname', 'serialno', 'status', 'linev', 'linefreq', 'loadpct', 'battv', 'bcharge'];
 
-  executeCmd('apcaccess', function(err, response) {
-    if (err) {
-      console.error(err);
-    }
-    else {
-      // console.log(response);
-      var lines = response.trim().split("\n");
+    executeCmd('apcaccess', function(err, response) {
+      if (err) {
+        console.error(err);
+      }
+      else {
+        // console.log(response);
+        var lines = response.trim().split("\n");
+ 
+        // loop over every line
+        lines.forEach(function (line) {
+          // assign values
+          var stats = line.split(' : ');
+          var label = stats[0].toLowerCase();
+          var value = stats[1];
 
-      // loop over every line
-      lines.forEach(function (line) {
-        // assign values
-        var stats = line.split(' : ');
-        var label = stats[0].toLowerCase();
-        var value = stats[1];
-
-        // remove surrounding spaces
-        label = label.replace(/(^\s+|\s+$)/g, '');
-        // if found as wanted value, store it
-        if (wanted.indexOf(label) > -1) {
-          value = value.replace(/(^\s+|\s+$)/g, '');
-          if (label == 'upsname') {
-            upsname = value;
+          // remove surrounding spaces
+          label = label.replace(/(^\s+|\s+$)/g, '');
+          // if found as wanted value, store it
+          if (wanted.indexOf(label) > -1) {
+            value = value.replace(/(^\s+|\s+$)/g, '');
+            if (label == 'upsname') {
+              upsname = value;
+            }
+            // check if value is known, if not store and publish value
+            if (curvalues[label] != value) {
+              curvalues[label] = value;
+              // console.log(value+" changed!");
+              // publish value
+              mclient.publish(topic+upsname+'/'+label, value, {retain: true});
+              if (err) throw err;
+            }
           }
-          // check if value is known, if not store and publish value
-          if (curvalues[label] != value) {
-            curvalues[label] = value;
-            // console.log(value+" changed!");
-            // publish value
-            mclient.publish(topic+upsname+'/'+label, value, {retain: true});
-            if (err) throw err;
-          }
-        }
-      });
-    }
-    // console.log(curvalues);
-    setTimeout(poll, pollint);
-  })
+        });
+      }
+      // console.log(curvalues);
+      setTimeout(poll, pollint);
+    })
 }
 
 // start plugin
